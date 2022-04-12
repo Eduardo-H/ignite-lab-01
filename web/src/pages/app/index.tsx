ff404f37f2aa724e09c79df7eedfd22abccb4d3f
Eduardo <edu-henrique2014@hotmail.com>
2022-04-11 22:12:37 -0300

    Fourth class | Graphql and Authentication

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,25 +1,33 @@
-import { getAccessToken, useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useGetProductsQuery, useMeQuery } from '../../graphql/generated/graphql';
+import { getServerPageGetProducts, ssrGetProducts } from '../../graphql/generated/page';
+import { withApollo } from '../../lib/withApollo';
 
-export default function Home() {
+function Home({ data }) {
   const { user } = useUser();
+  const { data: me } = useMeQuery();
 
   return (
     <div>
       <h1>Welcome to the app!</h1>
 
       <pre>
-        {JSON.stringify(user, null, 2)}
+        {JSON.stringify(me, null, 2)}
       </pre>
     </div>
   )
 }
 
 export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ({ req, res }) => {
-    console.log(getAccessToken(req, res))
+  getServerSideProps: async (ctx) => {
+    // return getServerPageGetProducts(null, ctx);
 
     return {
       props: {}
     }
   }
 });
+
+export default withApollo(
+  ssrGetProducts.withPage()(Home)
+);
